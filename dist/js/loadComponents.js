2ca760b5027fcd5dc86929d09879b5f84c1d10f2
bluhashark <arp45866745@gmail.com>
2025-12-26 04:04:44 +0800

    10連結

--- dist/js/loadComponents.js
+++ dist/js/loadComponents.js
@@ -21,7 +21,7 @@
 
     document.addEventListener("DOMContentLoaded", () => {
         // 請確保 header.html 和 footer.html 與引用此 script 的 HTML 檔案位於同一層目錄
-        loadComponent("./header.html", "header");
-        loadComponent("./footer.html", "footer");
+        loadComponent("./parts/header.html", "header");
+        loadComponent("./parts/footer.html", "footer");
     });
 })();
